5481c3dbd1f71b15c1edc09021f675546c893c5f
AlexMurmelstein <armurm@gmail.com>
2022-07-11 17:57:30 +0300

    301, 302, csrf tweak

--- routes/admin.js
+++ routes/admin.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 const express = require('express');
 
+const { check, body } = require('express-validator');
+
 const adminController = require('../controllers/admin');
 const isAuth = require('../middleware/is-auth');
 
@@ -14,11 +16,31 @@ router.get('/add-product', isAuth, adminController.getAddProduct);
 router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post(
+  '/add-product',
+  isAuth,
+  [
+    body('title').isString().isLength({ min: 3 }).trim(),
+    body('imageUrl').isURL(),
+    body('price').trim().isFloat().trim(),
+    body('description').isLength({ min: 7, max: 20 }).trim(),
+  ],
+  adminController.postAddProduct
+);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post(
+  '/edit-product',
+  isAuth,
+  [
+    body('title').isAlphanumeric().isLength({ min: 3 }).trim(),
+    body('imageUrl').isURL(),
+    body('price').trim().isFloat().trim(),
+    body('description').isLength({ min: 7, max: 20 }).trim(),
+  ],
+  adminController.postEditProduct
+);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
