65464db51c7dc33004f731c173fe320cb2d0b3a7
AlexMurmelstein <armurm@gmail.com>
2022-07-18 17:28:55 +0300

    331

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const PDFDocument = require('pdfkit');
 const Product = require('../models/product');
 const Order = require('../models/order');
 const errorHandler = require('../util/error');
@@ -143,13 +144,16 @@ exports.getInvoices = (req, res, next) => {
       }
       const invoiceName = `invoice-${orderId}.pdf`;
       const invoicePath = path.join('data', 'invoices', invoiceName);
-      const file = fs.createReadStream(invoicePath);
+      const pdfDoc = new PDFDocument();
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader(
         'Content-Disposition',
         `attachment; filename=${invoiceName}`
       );
-      file.pipe(res);
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+      pdfDoc.text('Hello world!');
+      pdfDoc.end();
     })
     .catch(err => {
       return next(errorHandler(err));
